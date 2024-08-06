9fcac1d200adbe4ea674b18167cbb641ec8f0e1e
JosephFrost <josephnina16forjob@gmail.com>
2024-08-06 06:07:59 -0400

    security in process

--- client/javascript/login.js
+++ client/javascript/login.js
@@ -1,9 +1,12 @@
-// iniciarSesionBtn = document.getElementById('iniciarSesion');
 
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 
+const error = document.querySelector('.error');
+
 function Login() {
+
+    console.log(email.value);
 
     const data = {
         email: email.value,
@@ -18,16 +21,17 @@ function Login() {
 
     fetch('http://localhost:4090/api/auth/login',config).then(response=>{
         if(!response.ok){
-            throw new Error('Error al crear el usuario')
+            throw new Error('Error al logear el usuario')
         }
         return response.json();
     })
     .then(data=>{
         // Aqui obtenemos el token en la variable data:
-
         console.log('token creado correctamente', data);
+        error.classList.add('none');
 
     }).catch(err=>{
         console.log('Hubo un error en el fectch ',err)
+        error.classList.remove('none');
     })
 };
